ca396d048d88cfdcc90eb2913764d74fffb55261
Vasanth Kumar <mybusiness.vkrs@gmail.com>
2025-03-25 22:25:07 +0530

    All are clear

---
+++ script.js
@@ -0,0 +1,42 @@
+function handleSignup() {
+  window.location.href = "./signup.html";
+}
+function handleLogin() {
+  window.location.href = "./index.html";
+}
+
+function handleSignUpValidation(event) {
+  event.preventDefault();
+
+  const fullname = document.querySelector("#fullname").value;
+  const username = document.querySelector("#username").value;
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/signup/",
+        { fullname, username, email, password },
+        { withCredentials: true }
+      );
+
+      if (!response.status === 200) {
+        throw new Error(`HTTP Error! Status: ${response.status}`);
+      }
+
+      const data = await response.data;
+
+      alert(data.message);
+
+      if (data.message === "✅ Account created successfully") {
+        window.location.href = "home.login";
+      } else {
+        window.location.href = "home.signup";
+      }
+    } catch (err) {
+      console.error("Error", err);
+    }
+  };
+  fetchData();
+}
